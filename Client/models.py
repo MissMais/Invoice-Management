a4f693f2a44332030751adcfcdba694b96ab8ac0
Mehtabalam04 <mehtabalamansari235@gmail.com>
2024-06-12 15:22:00 +0530

    Client and Invoice Model Creation

--- Client/models.py
+++ Client/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from Auth_user.models import CoreUser
 
 # Create your models here.
+
+
+
+class Client(models.Model):
+    client_id = models.AutoField(primary_key=True)
+    client_name = models.CharField(max_length=255,unique=True)
+    user_id = models.ForeignKey(CoreUser,on_delete=models.CASCADE,null=True)
+    company_address = models.CharField(max_length=255)
+
+
+class Invoice(models.Model):
+    invoice_id = models.AutoField(primary_key=True)
+    client_id = models.ForeignKey(Client,on_delete=models.CASCADE,null=True)
+    due_date = models.DateField()
+    total_amount = models.IntegerField()
+    status = models.CharField(max_length=255)
+
+
+
